0798b70d1b5313296818e200c4172c37cc45688a
marcioalves71 <marcioalves71@gmail.com>
2025-03-03 09:19:52 -0300

    aula de operadores

---
+++ tipos-variaveis/src/Operadores.java
@@ -0,0 +1,10 @@
+public class Operadores {
+
+    public static void main(String[] args) {
+        int i = 6;
+        int n = 6;
+
+        boolean resultado = i==n?true:false;
+        System.out.println(resultado);
+    }
+}
--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,11 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.print(numeroCurto2);
+
     }
 }

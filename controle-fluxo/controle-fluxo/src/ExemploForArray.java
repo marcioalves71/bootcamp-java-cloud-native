5b67e5ebf32a9fbd66c606732e0bf1892f98481d
marcioalves71 <marcioalves71@gmail.com>
2025-03-04 09:03:23 -0300

    commit aula sobre for com array

---
+++ controle-fluxo/controle-fluxo/src/ExemploForArray.java
@@ -0,0 +1,11 @@
+public class ExemploForArray {
+
+    public static void main(String[] args) {
+        
+        String alunos[] = {"Marcio", "Isabel", "Isabela", "Anne"};
+
+        for(int i=0; i<alunos.length; i++) {
+            System.out.println("O aluno do indice " + i + " e " + alunos[i]);
+        }
+    }
+}

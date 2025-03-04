972de61d6b0a7a7e95bd19b4bb279c26a235e0b6
marcioalves71 <marcioalves71@gmail.com>
2025-03-04 07:09:53 -0300

    meu segundo commit projeto conta banco

---
+++ conta-banco/ContaBanco/src/entities/ContaTerminal.java
@@ -0,0 +1,12 @@
+package entities;
+public class ContaTerminal {
+
+    private int numero;
+    private String agencia;
+    private String nomeCliente;
+    private double saldo;
+
+    public void ContaTerminal(){
+        
+    }
+}

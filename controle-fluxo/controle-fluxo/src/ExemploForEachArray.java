f07d105de04ab96d44c190d4b60e564671120a11
marcioalves71 <marcioalves71@gmail.com>
2025-03-04 09:31:05 -0300

    commit estudo while

---
+++ controle-fluxo/controle-fluxo/src/ExemploForEachArray.java
@@ -0,0 +1,11 @@
+public class ExemploForEachArray {
+
+    public static void main(String[] args) {
+        
+        String alunos[] = {"Marcio", "Isabel", "Isabela", "Anne"};
+
+        for (String aluno : alunos) {
+            System.out.println(aluno);
+        }
+    }
+}

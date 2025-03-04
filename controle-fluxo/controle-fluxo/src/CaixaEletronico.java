41f859eef7dd72a97000d679ad399b7a6991dfbb
marcioalves71 <marcioalves71@gmail.com>
2025-03-04 07:37:51 -0300

    commit exercicio caixa eletronico

---
+++ controle-fluxo/controle-fluxo/src/CaixaEletronico.java
@@ -0,0 +1,14 @@
+public class CaixaEletronico {
+    public static void main(String[] args) {
+        double saldo = 25.00;
+        double valorSolicitado = 17.00;
+
+        if (valorSolicitado < saldo) {
+            saldo = saldo - valorSolicitado;
+        }
+
+        System.out.println(saldo);
+    
+}
+
+}
